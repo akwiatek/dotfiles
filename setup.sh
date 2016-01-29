b9b20f51dda3bf71b937f2905841bc2690c04e0b
Adam Kwiatek <adamkw@kainos.com>
2016-01-29 10:35:12 +0000

    pull_git_repositories

--- setup.sh
+++ setup.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
 
+pull_git_repositories() {
+    for d in *; do
+        echo "$d"
+        cd "$d"
+        git pull
+        cd -
+    done
+}
+
 cd ~
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+[ -d ~/.oh-my-zsh/custom/plugins/ ] || mkdir -p ~/.oh-my-zsh/custom/plugins/
+cd ~/.oh-my-zsh/custom/plugins/
+git clone https://github.com/zsh-users/zsh-completions
+
+pull_git_repositories
+
+rm --force ~/.zcompdump*
+
 [ -d ~/src/git-extensions/ ] || mkdir -p ~/src/git-extensions/
 cd ~/src/git-extensions/
-# install git plugins
 git clone https://github.com/mhagger/git-imerge
 git clone https://github.com/tkrajina/git-plus
 
-# upgrade git plugins
-for d in *; do
-    echo "$d"
-    cd "$d"
-    git pull
-    cd -
-done
+pull_git_repositories
 
 [ -d ~/.vim/undo/ ] || mkdir -p ~/.vim/undo/
 [ -d ~/.vim/swaps/ ] || mkdir -p ~/.vim/swaps/
@@ -24,13 +34,14 @@ done
 cd ~/.vim/opt/
 git clone https://github.com/tpope/vim-pathogen.git
 
+pull_git_repositories
+
 [ -d ~/.vim/autoload/ ] || mkdir -p ~/.vim/autoload/
 cd ~/.vim/autoload/
 [ -L ./pathogen.vim ] || ln --symbolic ../opt/vim-pathogen/autoload/pathogen.vim ./pathogen.vim
 
 [ -d ~/.vim/bundle/ ] || mkdir -p ~/.vim/bundle/
 cd ~/.vim/bundle/
-# install vim plugins
 git clone https://github.com/godlygeek/csapprox
 git clone https://github.com/ctrlpvim/ctrlp.vim
 git clone https://github.com/gregsexton/gitv
@@ -53,14 +64,11 @@ git clone https://github.com/skwp/greplace.vim
 git clone https://github.com/nathanaelkane/vim-indent-guides
 git clone https://github.com/vim-scripts/camelcasemotion
 git clone https://github.com/rking/ag.vim
+git clone https://github.com/mileszs/ack.vim
 
-# upgrade vim plugins
-for d in *; do
-    echo "$d"
-    cd "$d"
-    git pull
-    cd -
-done
+pull_git_repositories
 
 [ -d ~/.vim/syntax/ ] || mkdir -p ~/.vim/syntax/
 curl http://www.haproxy.org/download/contrib/haproxy.vim > ~/.vim/syntax/haproxy.vim
+
+unset -f pull_git_repositories
