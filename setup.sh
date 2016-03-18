21c430b8e84c57b4dbddbe20c85241397f55deab
Adam Kwiatek <kwiatek@iv.pl>
2016-03-18 00:30:26 +0000

    setup: conditional actions

--- setup.sh
+++ setup.sh
@@ -11,19 +11,19 @@ pull_git_repositories() {
 
 [ -d ~/src/git-extensions/ ] || mkdir -p ~/src/git-extensions/
 cd ~/src/git-extensions/
-git clone https://github.com/mhagger/git-imerge
-git clone https://github.com/tkrajina/git-plus
-git clone https://github.com/unixorn/git-extra-commands
+[ -d git-imerge ] || git clone https://github.com/mhagger/git-imerge
+[ -d git-plus ] || git clone https://github.com/tkrajina/git-plus
+[ -d git-extra-commands ] || git clone https://github.com/unixorn/git-extra-commands
 
 pull_git_repositories
 
 cd ~
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+[ -d .oh-my-zsh ] || sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 [ -d ~/.oh-my-zsh/custom/plugins/ ] || mkdir -p ~/.oh-my-zsh/custom/plugins/
 cd ~/.oh-my-zsh/custom/plugins/
-ln -s ~/src/git-extensions/git-extra-commands
-git clone https://github.com/zsh-users/zsh-completions
+[ -L git-extra-commands ] || ln --symbolic ~/src/git-extensions/git-extra-commands git-extra-commands
+[ -d zsh-completions ] || git clone https://github.com/zsh-users/zsh-completions
 
 pull_git_repositories
 
@@ -34,45 +34,45 @@ rm --force ~/.zcompdump*
 
 [ -d ~/.vim/opt/ ] || mkdir -p ~/.vim/opt/
 cd ~/.vim/opt/
-git clone https://github.com/tpope/vim-pathogen.git
+[ -d vim-pathogen ] || git clone https://github.com/tpope/vim-pathogen.git
 
 pull_git_repositories
 
 [ -d ~/.vim/autoload/ ] || mkdir -p ~/.vim/autoload/
 cd ~/.vim/autoload/
-[ -L ./pathogen.vim ] || ln --symbolic ../opt/vim-pathogen/autoload/pathogen.vim ./pathogen.vim
+[ -L pathogen.vim ] || ln --symbolic ../opt/vim-pathogen/autoload/pathogen.vim pathogen.vim
 
 [ -d ~/.vim/bundle/ ] || mkdir -p ~/.vim/bundle/
 cd ~/.vim/bundle/
-git clone https://github.com/godlygeek/csapprox
-git clone https://github.com/ctrlpvim/ctrlp.vim
-git clone https://github.com/gregsexton/gitv
-git clone https://github.com/othree/javascript-libraries-syntax.vim
-git clone https://github.com/scrooloose/nerdcommenter
-git clone https://github.com/scrooloose/nerdtree
-git clone https://github.com/Xuyuanp/nerdtree-git-plugin
-git clone https://github.com/scrooloose/syntastic
-git clone https://github.com/mbbill/undotree
-git clone https://github.com/alvan/vim-closetag
-git clone https://github.com/flazz/vim-colorschemes
-git clone https://github.com/easymotion/vim-easymotion
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/tpope/vim-git
-git clone https://github.com/airblade/vim-gitgutter
-git clone https://github.com/vim-scripts/JavaDecompiler.vim.git
-git clone https://github.com/keith/tmux.vim
-git clone https://github.com/sheerun/vim-polyglot
-git clone https://github.com/skwp/greplace.vim
-git clone https://github.com/nathanaelkane/vim-indent-guides
-git clone https://github.com/vim-scripts/camelcasemotion
-git clone https://github.com/mileszs/ack.vim
-git clone https://github.com/vim-scripts/argwrap.vim
-git clone https://github.com/vim-scripts/Merginal
-git clone https://github.com/bronson/vim-trailing-whitespace
+[ -d csapprox ] || git clone https://github.com/godlygeek/csapprox
+[ -d ctrlp.vim ] || git clone https://github.com/ctrlpvim/ctrlp.vim
+[ -d gitv ] || git clone https://github.com/gregsexton/gitv
+[ -d javascript-libraries-syntax.vim ] || git clone https://github.com/othree/javascript-libraries-syntax.vim
+[ -d nerdcommenter ] || git clone https://github.com/scrooloose/nerdcommenter
+[ -d nerdtree ] || git clone https://github.com/scrooloose/nerdtree
+[ -d nerdtree-git-plugin ] || git clone https://github.com/Xuyuanp/nerdtree-git-plugin
+[ -d syntastic ] || git clone https://github.com/scrooloose/syntastic
+[ -d undotree ] || git clone https://github.com/mbbill/undotree
+[ -d vim-closetag ] || git clone https://github.com/alvan/vim-closetag
+[ -d vim-colorschemes ] || git clone https://github.com/flazz/vim-colorschemes
+[ -d vim-easymotion ] || git clone https://github.com/easymotion/vim-easymotion
+[ -d vim-fugitive ] || git clone https://github.com/tpope/vim-fugitive.git
+[ -d vim-git ] || git clone https://github.com/tpope/vim-git
+[ -d vim-gitgutter ] || git clone https://github.com/airblade/vim-gitgutter
+[ -d JavaDecompiler.vim ] || git clone https://github.com/vim-scripts/JavaDecompiler.vim.git
+[ -d tmux.vim ] || git clone https://github.com/keith/tmux.vim
+[ -d vim-polyglot ] || git clone https://github.com/sheerun/vim-polyglot
+[ -d greplace.vim ] || git clone https://github.com/skwp/greplace.vim
+[ -d vim-indent-guides ] || git clone https://github.com/nathanaelkane/vim-indent-guides
+[ -d camelcasemotion ] || git clone https://github.com/vim-scripts/camelcasemotion
+[ -d ack.vim ] || git clone https://github.com/mileszs/ack.vim
+[ -d argwrap.vim ] || git clone https://github.com/vim-scripts/argwrap.vim
+[ -d Merginal ] || git clone https://github.com/vim-scripts/Merginal
+[ -d vim-trailing-whitespace ] || git clone https://github.com/bronson/vim-trailing-whitespace
 
 pull_git_repositories
 
 [ -d ~/.vim/syntax/ ] || mkdir -p ~/.vim/syntax/
-curl http://www.haproxy.org/download/contrib/haproxy.vim > ~/.vim/syntax/haproxy.vim
+curl --silent --show-error http://www.haproxy.org/download/contrib/haproxy.vim > ~/.vim/syntax/haproxy.vim
 
 unset -f pull_git_repositories
