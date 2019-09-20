9a7b1bf3af9422a34175905c591fa46d37ea243d
Adam Kwiatek <kwiatek.adam@poczta.fm>
2019-09-20 21:29:48 +0200

    conflicted

--- setup.sh
+++ setup.sh
@@ -99,6 +99,7 @@ safe_cd ~/.vim/bundle/
 try_git_clone https://github.com/airblade/vim-gitgutter
 try_git_clone https://github.com/alvan/vim-closetag
 try_git_clone https://github.com/aserebryakov/filestyle
+try_git_clone https://github.com/christoomey/vim-conflicted
 try_git_clone https://github.com/craigemery/vim-autotag
 try_git_clone https://github.com/ctrlpvim/ctrlp.vim
 try_git_clone https://github.com/DavidEGx/ctrlp-smarttabs
