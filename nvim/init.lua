196550ea84854f86e5dce7012399b06e47c868f6
Adam Kwiatek <kwiatek.adam@poczta.fm>
2025-01-11 16:46:53 +0100

    vim tagbar plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -374,14 +374,6 @@ else
     }
 end
 
-vim.g.tagbar_width = 50
--- 0: Use the width of the longest currently visible tag.
-vim.g.tagbar_zoomwidth = 0
--- 1: Show absolute line numbers.
-vim.g.tagbar_show_linenumbers = 1
-vim.g.tagbar_autopreview = 1
-vim.g.tagbar_previewwin_pos = 'rightbelow'
-
 vim.g.used_javascript_libs = 'jquery,underscore,angularjs,angularui,angularuirouter,jasmine'
 
 -- Sort entries by name rather than by chronological order
---
+++ nvim/plugin/tagbar.lua
@@ -0,0 +1,12 @@
+--[[
+-- Plugin https://github.com/majutsushi/tagbar
+--
+--]]
+--
+vim.g.tagbar_width = 50
+-- 0: Use the width of the longest currently visible tag.
+vim.g.tagbar_zoomwidth = 0
+-- 1: Show absolute line numbers.
+vim.g.tagbar_show_linenumbers = 1
+vim.g.tagbar_autopreview = 1
+vim.g.tagbar_previewwin_pos = 'rightbelow'
